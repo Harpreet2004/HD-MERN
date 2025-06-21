16dff2d67f00ebb4b58b7cd27a9e1980f70fdf4f
harpreet2004 <harpreetsingh5066@gmail.com>
2025-06-21 12:30:58 +0530

    FIX: cors error

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,13 @@ const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(cors())
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        methods: ["GET","POST","PUT","DELETE"],
+        credentials: true
+    })
+);
 app.use(express.json())
 app.use("/api/notes", notesRouter)
 app.use("/api/users", userRouter)
